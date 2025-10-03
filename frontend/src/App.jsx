753031aaf8d05937a803710075396f4304f7f123
Xrid-driX <heldrixcute@gmail.com>
2025-10-03 09:48:23 +0800

    update app jsx | added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,18 @@
-import { useState } from 'react';
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return (
-   <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start">
-    {/*header*/}
-    <header className="w-full bg-indigo-600 text-white p-4 shadow-md">
-      <h1 className="text-2x1 font-bold text-center">Inventory app for robotics</h1>
-    </header>
+  return(
+    <Router>
+      <Routes>
+        <Route path= "/" element={<Login />} />
+        <Route path= "/dashboard" element={< Dashboard/>} />
 
-    {/* Main header gang */}
-    <main className="w-full max-w-4xl p-6">
-      <div className="bg-white shadow rounded-lg p-6">
-        <h2 className="text-xl font-semibold mb-4">Dashboard</h2>
-        <p className="text-gray-600">
-          Welcome! Inventory Items will appear here.
-        </p>
-      </div>
-    </main>
-   </div> 
-   
-  );
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
